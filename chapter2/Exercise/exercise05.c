c19c5a3e86ebfca4e0fe66f241b126f67303c2c8
Aspartame <1135975764@qq.com>
2022-07-19 19:10:19 +0800

    chapter2练习代码

---
+++ chapter2/Exercise/exercise05.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void br();
+void ic();
+int main(void){
+    br();
+    printf(",");
+    ic();
+    printf("\n");
+    br();
+    printf("\n");
+    ic();
+    printf("\n");
+    return 0;
+}
+void br(){
+    printf("Brazil, Russia");
+}
+void ic(){
+    printf("India, China");
+}
---
+++ chapter2/Exercise/exercise06.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+    int toes = 10;
+    int toes_2,toes_3;
+    toes_2 = toes*toes;
+    toes_3 = toes*toes*toes;
+    printf("toes = %d,toes_2 = %d,toes_3=%d",toes,toes_2,toes_3);
+
+    return 0;
+}
